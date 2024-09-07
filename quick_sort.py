34a1417eea9d4abf48ca247fcbe8badf049caebd
adrianUC <nichita@me.com>
2024-09-07 17:18:02 -0400

    changes

--- quick_sort.py
+++ quick_sort.py
@@ -1,5 +1,8 @@
 import random
 from enum import Enum
+import time
+import sys
+
 class PivotSelection(Enum):
     RANDOM = 1
     DETERMINISTIC = 2
@@ -35,10 +38,58 @@ def quick_sort(source, pivot_selection=PivotSelection.RANDOM):
     return copy
 
 # Example usage:
-source = [9, 5, 1, 8, 3, 7, 4, 6, 2]
-sortedRandom = quick_sort(source, pivot_selection=PivotSelection.RANDOM)
-sortedDeterministic = quick_sort(source, pivot_selection=PivotSelection.DETERMINISTIC)
+# source = [9, 5, 1, 8, 3, 7, 4, 6, 2]
+# sortedRandom = quick_sort(source, pivot_selection=PivotSelection.RANDOM)
+# sortedDeterministic = quick_sort(source, pivot_selection=PivotSelection.DETERMINISTIC)
 
-print(source)
-print(sortedRandom)
-print(sortedDeterministic)
+# print(source)
+# print(sortedRandom)
+# print(sortedDeterministic)
+
+array_length = 10000
+
+# 1. A random array of 100000 elements
+random_array = random.sample(range(1, array_length + 1), array_length)
+
+# 2. A sorted array
+already_sorted = quick_sort(random_array)
+
+# 3. A reverse sorted array
+reverse_sorted = already_sorted[::-1]
+
+# 4. An array with repeated elements: 100 elements randomly selected from the first 25 elements of the random array
+repeated_elements = [random.choice(random_array[:50]) for _ in range(array_length)]
+
+# 5. An array with all elements equal
+all_elements_equal = [1 for _ in range(array_length)]
+
+
+# Ok, all of our arrays are ready. Let's compare the running time of quick_sort with random and deterministic pivot selection for each of the arrays.
+
+
+def test_quick_sort(name, array, pivot_selection):
+    start_time = time.time()
+    sorted_array = quick_sort(array, pivot_selection=pivot_selection)
+    end_time = time.time()
+    print(name, "with", pivot_selection,": ", end_time - start_time)
+    return end_time - start_time
+
+def test_both_pivot_selections(name, array):
+    random_time = test_quick_sort(name, array, PivotSelection.RANDOM)
+    deterministic_time = test_quick_sort(name, array, PivotSelection.DETERMINISTIC)
+    diff_in_ms = (random_time - deterministic_time) * 1000
+    if diff_in_ms > 0:
+        print("Deterministic pivot selection is faster by ", diff_in_ms, " milliseconds")
+    else:
+        print("Random pivot selection is faster by ", -diff_in_ms, " milliseconds")
+    print()
+
+# Increase the recursion limit, otherwise the code will throw RecursionError for large arrays
+sys.setrecursionlimit(20000)
+
+# Random array
+test_both_pivot_selections("Random array", random_array)
+test_both_pivot_selections("Already sorted array", already_sorted)
+test_both_pivot_selections("Reverse sorted array", reverse_sorted)
+test_both_pivot_selections("Repeated elements array", repeated_elements)
+test_both_pivot_selections("All elements equal array", all_elements_equal)
